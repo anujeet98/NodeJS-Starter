608ced37b3da7b93231b89b68823a9d136025f4f
Anujeet Swain <anujeet98@gmail.com>
2023-11-23 02:01:28 +0530

    added routing and send response for different routess

--- createServer.js
+++ createServer.js
@@ -2,7 +2,38 @@
 const http = require('http');
 
 const server = http.createServer((req, res) =>{
-    console.log('Anujeet');
+    // console.log('Anujeet');
+    // process.exit();
+    // console.log(req.url, req.method, req.headers);
+
+    const url = req.url;
+    if(url === '/home'){
+        res.setHeader('Content-Type', 'text/HTML');
+        res.write('<html>');
+        res.write('<head><title>Node JS server application</title></head>');
+        res.write('<body><h1>Welcome home</h1></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+    if(url === '/about'){
+        res.setHeader('Content-Type', 'text/HTML');
+        res.write('<html>');
+        res.write('<head><title>Node JS server application</title></head>');
+        res.write('<body><h1>Welcome to About us page</h1></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+    if(url === '/node'){
+        res.setHeader('Content-Type', 'text/HTML');
+        res.write('<html>');
+        res.write('<head><title>Node JS server application</title></head>');
+        res.write('<body><h1>Welcome to my node JS application</h1></body>');
+        res.write('</html>');
+        res.end();
+    }
+    // res.write('<h1>sdsdf</h1>'); //gives error after 1 request: write after end error thrown
 });
 
 server.listen(4000);
