8a398a37298a4b28d447faaa8aa8c5e53736ba39
Anujeet Swain <anujeet98@gmail.com>
2023-11-23 01:02:10 +0530

    create server and listen to port

---
+++ createServer.js
@@ -0,0 +1,8 @@
+
+const http = require('http');
+
+const server = http.createServer((req, res) =>{
+    console.log('Anujeet');
+});
+
+server.listen(4000);
--- first-app.js
+++ first-app.js
@@ -1,5 +1,65 @@
-console.log('Hello from nodejs');
+// 
+console.log('a');
+console.log('b');
+new Promise((resolve,reject) => {
+	setTimeout(() => {
+		console.log('c');
+		resolve();
+	}, 3000);
+}).then(()=>{
+    return new Promise((resolve,reject) =>{
+        setTimeout(()=>{
+            console.log('d');
+            resolve();
+        },0);
+    })
+}).then(()=>{console.log('e')});
 
-const fs = require('fs');
 
-fs.writeFileSync('hello.txt','Hello from nodejs');
+// console.log('a');
+
+// console.log('b');
+
+// setTimeout(() => console.log('c'), 3000)
+
+// console.log('d');
+
+
+// console.log('a');
+// console.log('b');
+
+// let fun = async () => {
+//     await new Promise((resolve,reject) => setTimeout(() => {
+//         console.log('c'); 
+//         resolve()
+//     }, 3000));
+//     await new Promise((resolve,reject) => setTimeout(()=> {
+//         console.log('d');
+//         resolve();
+//     }, 0));
+//     console.log('e');
+// }
+
+// fun();
+
+
+// console.log('a');
+// console.log('b');
+
+// let funC = (letterToPrint) => {
+//     return new Promise((resolve,reject) => {
+//         setTimeout(() => {
+//             console.log(letterToPrint);
+//             resolve();
+//         }, 3000);
+//     })
+// };
+
+// let funD = new Promise((resolve,reject) => {
+//                 setTimeout(() => {
+//                     console.log("hello");
+//                     resolve();
+//                 }, 0);
+//             });
+
+// funC('c').then(funD).then(console.log('e'));
