d4250ab9aed434ddb0d842fb6e2c5cf7d264ce7b
Anujeet Swain <anujeet98@gmail.com>
2023-11-25 02:55:06 +0530

    chat app update code with express and local storage integration

--- chatAppFiles.js
+++ chatAppFiles.js
@@ -1,9 +1,22 @@
-const http = require('http');
-const route = require('./routes.js');
+// const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-console.log(route.someText);
+const loginRoute = require('./login');
+const chatPageRoute = require('./chatPage');
 
-const server = http.createServer(route.handler);
 
+const app = express();
 
-server.listen(3000);
+//parsebody
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(loginRoute);
+app.use(chatPageRoute);
+
+app.use((req,res) => {
+    res.status(404).send('<h1>Error 404: page not found</h1>');
+});
+
+app.listen(3000);
+
---
+++ chatPage.js
@@ -0,0 +1,92 @@
+const fs = require('fs');
+const express = require('express');
+
+const router = express.Router();
+
+let read = (req,res) =>{
+    fs.readFile('message.txt',(err,data)=>{
+        if(err){
+            console.log(err);
+        }
+
+
+        data = (data.length===0)?'No chats exist':data;
+        console.log("sending data: ");
+        let html = `<h1>${data}</h1>`;
+        html+='<form method="post" action="/">';
+        html+=`<input type="text" name="message" placeholder="message" id="message"><input type="hidden" name="username" id="username" value=""><input type="submit" value="send">`;
+        html+='</form>';
+        html+='<script>';
+        html+='document.getElementById("username").value = localStorage.getItem("username");';
+        html+="document.addEventListener('DOMContentLoaded', (e)=> {e.preventDefault(); document.getElementById('message').value = ''})";
+        html+='</script>';
+        res.send(html);
+    });
+}
+
+router.use('/',(req,res,next)=>{
+    if(req.body.message !== undefined && req.body.message !== ""){
+        console.log("message recieved to append", req.body);
+        fs.appendFile('message.txt', req.body.username+": "+req.body.message+";    \n",(err)=>{
+            if(err){
+                console.log(err);
+            }
+            read(req,res);
+        });
+    }
+    else{
+        read(req,res);
+    }
+});
+
+
+// let requestHandler = (req,res) => {
+//     const url = req.url;
+//     const post = req.method;
+//     if(url === '/'){
+
+//         return;
+//     }
+//     if(url === '/message' && post === "POST"){
+//         const body = [];
+//         req.on('data', (chunk)=>{
+//             body.push(chunk);
+//             //console.log(chunk);
+//         });
+//         return req.on('end', ()=>{
+//             const parsedBody = Buffer.concat(body).toString();
+//             console.log(parsedBody);
+
+//             const message = parsedBody.split("=")[1];
+
+//             fs.writeFile('message.txt',message,(err)=>{
+//                 res.statusCode = 302;
+//                 res.setHeader('location','/');
+//                 return res.end();
+//             });
+//         });
+    
+//     }
+//     res.setHeader('Content-Type', 'text/HTML');
+//     res.write('<html>');
+//     res.write('<head><title>Node JS server application</title></head>');
+//     res.write('<body><h1>Welcome to my node JS application</h1></body>');
+//     res.write('</html>');
+//     return res.end();
+// };
+
+
+// module.exports = requestHandler;
+
+// module.exports = {
+//     handler : requestHandler,
+//     someText : 'hello world'
+// };
+
+// module.exports.handler = requestHandler;
+// module.exports.someText = 'hello world';
+
+// exports.handler = requestHandler;
+// exports.someText = 'hello world';
+
+module.exports = router;
